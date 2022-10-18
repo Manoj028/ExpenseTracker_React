9d75a381d8e1359714765553c8c600b7e3049106
Manoj <103386321+Manoj028@users.noreply.github.com>
2022-10-18 23:01:02 +0530

    Add files via upload

---
+++ src/Components/Expenses/ExpensesChart.js
@@ -0,0 +1,27 @@
+import React from "react";
+import Chart from "../chart/Chart";
+
+const ExpenseChart = (props) => {
+  const charDataPoints = [
+    { label: "jan", value: 0 },
+    { label: "Feb", value: 0 },
+    { label: "Mar", value: 0 },
+    { label: "Apr", value: 0 },
+    { label: "May", value: 0 },
+    { label: "Jun", value: 0 },
+    { label: "jul", value: 0 },
+    { label: "Aug", value: 0 },
+    { label: "Sep", value: 0 },
+    { label: "Oct", value: 0 },
+    { label: "Nov", value: 0 },
+    { label: "Dec", value: 0 },
+  ];
+
+  for (const expense of props.expenses) {
+    const expenseMonth = expense.date.getMonth();
+    charDataPoints[expenseMonth].value += expense.amount;
+  }
+
+  return <Chart dataPoints = {charDataPoints}  />;
+};
+export default ExpenseChart;
